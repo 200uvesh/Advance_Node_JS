90c7436c8fdb486560449350fd0f87112f003230
Uvesh <200uvesh.ahmed@gmail.com>
2024-03-04 11:13:54 +0530

    zip method added

--- Streams/index.js
+++ Streams/index.js
@@ -5,16 +5,61 @@ const app = server()
 
 
 //Streams
-// const stream = require("stream")
-// const fs = require("fs")
+const fs = require("fs")
+const status = require("express-status-monitor")
 
 
 
+
+//middlieware
+app.use(status())
 
 //Routes
+
+//Home Route
 app.get("/" , (req , res)=>{
     res.send("Hii I am from HomePage")
 })
+
+// Data Route (Without Stream)
+app.get("/data" , (req , res)=>{
+    fs.readFile("./sample.txt" , (err , data)=>{
+        res.end(data)
+    })
+    
+})
+
+
+
+// Data Route (Witt the help of Stream)
+app.get("/dataStream" , (req , res)=>{
+    const stream = fs.createReadStream("./sample.txt" , "utf-8")
+    stream.on("data" , (chunk)=>{
+        res.write(chunk)
+    })
+    stream.on("end" , ()=>{
+        res.end()
+    })
+
+
+})
+
+
+// Create a function for converting a file into Zip with using Streams
+const zipMaker = function(){
+    const zlib = require("zlib")
+    console.log("me yha hu")
+fs.createReadStream("./sample.txt").pipe(
+    zlib.createGzip().pipe(fs.createWriteStream("./sample.zip"))
+)
+
+}
+
+// zipMaker()
+ 
+
+
+ 
 
 
 
